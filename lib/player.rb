60ac5eed97fa564bd7c11d130e67057ed79793fb
trillianjose <trillianjosea@gmail.com>
2019-04-17 22:09:10 +0200

    finish player

---
+++ lib/player.rb
@@ -0,0 +1,23 @@
+class Player
+  attr_reader :name, :character, :play
+
+  def initialize(name, character)
+    @name = name
+    @character = character
+    @play = []
+  end
+
+  def wins?(line)
+    line & @play == line
+  end
+
+  def get_choice(game_board)
+    puts "#{@name} select a number>>"
+    choice = gets.chomp.to_i
+    if (1..9).include?(choice)
+      game_board.is_fulled?(choice) ? puts("Try again, that spot has been taken already.\n\n") : choice
+    else
+      puts('Enter a valid number, please.')
+    end
+  end
+end
